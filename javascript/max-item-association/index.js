0dce3963c85c0e494cad49ab04a29c5adf506b71
di-i <55539864+di-i@users.noreply.github.com>
2021-03-04 10:46:12 +0300

    Added file index.js

---
+++ javascript/max-item-association/index.js
@@ -0,0 +1,63 @@
+function maxItemAssociation(associations) {
+    if (associations.length === 0) {
+      return [];
+    } else if (!Array.isArray(associations)) {
+      return 'The argument must be an array.';
+    }
+  
+    let dictionaryItemNeighbors = createDictionaryItemNeighbors(associations);
+  
+    let visitedItemsInDictionary = new Set();
+    let maxListOfItems = [];
+    for (let key of Object.keys(dictionaryItemNeighbors)) {
+      let currentListOfItems = [];
+      if (!visitedItemsInDictionary.has(key)) {
+        createAssociatesList(key, dictionaryItemNeighbors, currentListOfItems, visitedItemsInDictionary);
+        currentListOfItems.sort();
+        if (!maxListOfItems.length) {
+          maxListOfItems.push(...currentListOfItems);
+        } else if (currentListOfItems.length > maxListOfItems.length) {
+          maxListOfItems = currentListOfItems.slice();
+        } else continue;
+      }
+    }
+    return maxListOfItems;
+  }
+  
+  const createDictionaryItemNeighbors = (array) => {
+    let dictionary = {};
+    for (let [firstItem, secondItem] of array) {
+      if (firstItem == undefined && secondItem == undefined) return [];
+      else if (firstItem == undefined || secondItem == undefined) return [];
+      if (dictionary[firstItem] == undefined) dictionary[firstItem] = [];
+      if (dictionary[secondItem] == undefined) dictionary[secondItem] = [];
+      dictionary[firstItem].push(secondItem);
+      dictionary[secondItem].push(firstItem);
+    }
+    return dictionary;
+  };
+  
+  const createAssociatesList = (
+    key,
+    dictionaryItemNeighbors,
+    currentListOfItems,
+    visitedItemsInDictionary,
+  ) => {
+    if (!visitedItemsInDictionary.has(key)) {
+      visitedItemsInDictionary.add(key);
+      currentListOfItems.push(key);
+      for (let neighbour of dictionaryItemNeighbors[key]) {
+        if (visitedItemsInDictionary.has(neighbour)) continue;
+        createAssociatesList(
+          neighbour,
+          dictionaryItemNeighbors,
+          currentListOfItems,
+          visitedItemsInDictionary,
+        );
+      }
+    }
+    return currentListOfItems;
+  };
+  
+  module.exports = maxItemAssociation;
+  
